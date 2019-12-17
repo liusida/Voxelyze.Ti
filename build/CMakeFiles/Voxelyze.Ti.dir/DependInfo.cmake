8663d084e574550329b82bb0de1d07affd8600e3
Sida <Sida.Liu.1@uvm.edu>
2019-12-17 13:14:57 -0500

    init

---
+++ build/CMakeFiles/Voxelyze.Ti.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liusida/simulator/Voxelyze.Ti/main.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/main.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_Collision.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_Collision.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_External.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_External.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_LinearSolver.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_LinearSolver.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_Link.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_Link.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_Material.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_Material.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_MaterialLink.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_MaterialLink.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_MaterialVoxel.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_MaterialVoxel.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_MeshRender.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_MeshRender.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/VX_Voxel.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/VX_Voxel.cpp.o"
+  "/home/liusida/simulator/Voxelyze.Ti/src/Voxelyze.cpp" "/home/liusida/simulator/Voxelyze.Ti/build/CMakeFiles/Voxelyze.Ti.dir/src/Voxelyze.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/liusida/simulator/Voxelyze.Ti
+# Build directory: /home/liusida/simulator/Voxelyze.Ti/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
