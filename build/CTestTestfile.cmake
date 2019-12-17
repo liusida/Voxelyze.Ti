33174638980d9ef0f2f5981e97fca47ac8a85963
Sida Liu <me@liusida.com>
2019-12-17 13:30:46 -0500

    init

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liusida/simulator/Voxelyze.Ti
-# Build directory: /home/liusida/simulator/Voxelyze.Ti/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
